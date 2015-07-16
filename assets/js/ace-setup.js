254cabe41a1547b9a2ca904a8ec7532f3a9b7c16
Gary Chamberlain <gary@pro.graphics>
2015-07-15 23:29:27 -0400

    Supports up to three versions of an animation.

--- assets/js/ace-setup.js
+++ assets/js/ace-setup.js
@@ -1,5 +1,26 @@
 $(function() {
 
+		var $btnJson1 = $("#btnJson1");
+		var $btnJson2 = $("#btnJson2");
+		var $btnJson3 = $("#btnJson3");
+		var $btnUpdateAnim = $("#btnUpdateAnim");
+
+		var json1 = $("#editor").text();
+		var json2 = $("#editor2").text();
+		var json3 = $("#editor3").text();
+
+		$("#editor2").remove();
+		$("#editor3").remove();
+
+		if (json2.length == 0) {
+			$btnJson1.text("Reset");
+			$btnJson2.css("display", "none");
+			$btnJson3.css("display", "none");
+		}
+		else if (json3.length == 0) {
+			$btnJson3.css("display", "none");
+		}
+
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/clouds");
     editor.getSession().setMode("ace/mode/javascript");
@@ -7,9 +28,7 @@ $(function() {
 				maxLines: 10000
 		});
 		editor.focus();
-		
-		var $btnUpdateAnim = $("#btnUpdateAnim");
-		
+
     var throttleTimer = null;
     function throttle(fn, delay) {
 			var context = this, args = arguments;
@@ -20,7 +39,7 @@ $(function() {
 				}, delay);
 			}
     }
-		
+
 		var update = function() {
 			clearTimeout(throttleTimer);
 			$btnUpdateAnim.attr("disabled","disabled");
@@ -40,7 +59,7 @@ $(function() {
 				showError("<i>Error:</i><br><br>" + err.message);
 			}
 		};
-		
+
 		var showError = function(msg) {
 			var $animation = $("#animation");
 			$animation.html(msg);
@@ -48,14 +67,39 @@ $(function() {
 			$animation.removeAttr("class");
 			$animation.addClass("anim-error");
 		}
-		
+
 		var throttledUpdate = throttle(function() {	update(); }, 10000);
-		
+
 		var session = editor.getSession().on('change', function() {
 			$btnUpdateAnim.removeAttr("disabled");
+			var value = editor.getValue().trim();
+			$btnJson1.attr("disabled", "disabled");
+			$btnJson2.attr("disabled", "disabled");
+			$btnJson3.attr("disabled", "disabled");
+			if (value!==json1.trim()) $btnJson1.removeAttr("disabled");
+			if (value!==json2.trim()) $btnJson2.removeAttr("disabled");
+			if (value!==json3.trim()) $btnJson3.removeAttr("disabled");
 			throttledUpdate();
 		});
-		
+
+		$btnJson1.click(function() {
+			editor.setValue(json1, -1);
+			editor.focus();
+			update();
+		});
+
+		$btnJson2.click(function() {
+			editor.setValue(json2, -1);
+			editor.focus();
+			update();
+		});
+
+		$btnJson3.click(function() {
+			editor.setValue(json3, -1);
+			editor.focus();
+			update();
+		});
+
 		$btnUpdateAnim.click(function() {
 			editor.focus();
 			update();
